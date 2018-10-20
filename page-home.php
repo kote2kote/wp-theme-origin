dbc7b2fe7d192c96f73ad63f754c4386268f6a8f
kote2 <kote2kote2@gmail.com>
2018-10-20 16:44:57 +0900

    仮コーディング完了

--- page-home.php
+++ page-home.php
@@ -4,6 +4,5 @@
 */
 //get_template_part('content','hero');
 get_header(); ?>
-<h1>Home</h1>
-<p><a href="<?php bloginfo('url'); ?>/menu-list-east/">menu</a></p>
+
 <?php get_footer(); ?>
--- page-menu-list.php
+++ page-menu-list.php
@@ -4,5 +4,256 @@
 */
 //get_template_part('content','hero');
 get_header(); ?>
+<section class="hero flex">
+<h2 class="heading">メニュー</h2>
+</section>
 
+<section class="breadcrumb">
+  <div class="section-content">
+    <ul class="breadcrumb__menu_list flex">
+      <li class="item"><a href="">link</a></li>
+      <li class="item"><a href="">link</a></li>
+      <li class="item"><a href="">link</a></li>
+    </ul>
+  </div>
+</section>
+
+<section class="food_menu">
+
+  <div class="tab flex">
+    <span class="east cm0">関東メニュー</span>
+    <span class="west cm0">関西メニュー</span>
+  </div>
+
+  <div class="food cm0">
+    <div class="section-content">
+      <div class="search_menu cm0">
+        <h3 class="title cm0">メニュー検索</h3>
+        <div class="search_menu__content">
+          <ul class="menu_list flex">
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="search_menu allergen cm0">
+        <h3 class="title cm0">アレルゲン検索</h3>
+        <div class="search_menu__content">
+          <ul class="menu_list flex">
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+            <li class="item">
+              <img src="https://placehold.jp/76x67.png" alt="">
+              <span>aa</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <p class="align-center">※アレルゲン検索機能は『Internet Explorer 8』には対応しておりません。</p>
+      <div class="search_btn btn align-center"><button>検索</button></div>
+      <div class="search_select align-center">52件が該当しました
+        <span class="search_select_box">
+        <select name="example">
+          <option value="サンプル1">サンプル1</option>
+          <option value="サンプル2">サンプル2</option>
+          <option value="サンプル3">サンプル3</option>
+        </select>
+        </span>
+      </div>
+      <p class="warning">各メニューには栄養成分の数値を「エネルギー / タンパク質 / 脂質 / 炭水化物 / ナトリウム / 食塩相当量」の順で記載をしております。</p>
+
+    </div>
+  </div>
+
+</section>
+
+<section class="main_menu">
+  <div class="section-content cm0">
+    <ul class="main_menu__list row">
+
+      <li class="item col-3 col-4-lg col-6-md col-12-sm">
+        <div class="item_content cm0">
+          <a href="#" class="link">
+            <div class="figure">
+              <figure><img src="https://placehold.jp/900x600.png" alt=""></figure>
+              <span class="label cm0">new</span>
+            </div>
+            <div class="line"></div>
+            <div class="detail">
+              <h4 class="menu_name">粒マスタードハンバーグ弁当</h4>
+              <p>
+                <span class="kakaku">本体価格 : 741円/1個</span>
+                <span class="zeikomi">(税込 : 800円/1個)</span>
+                <span class="energy">1210kcal / 45.1g / 56.1g / 124.9g / 1919mg / 4.9g</span>
+              </p>
+            </div>
+          </a>
+        </div>
+      </li>
+
+      <li class="item col-3 col-4-lg col-6-md col-12-sm">
+        <div class="item_content cm0">
+          <a href="#" class="link">
+            <div class="figure">
+              <figure><img src="https://placehold.jp/900x600.png" alt=""></figure>
+              <span class="label cm0">new</span>
+            </div>
+            <div class="line"></div>
+            <div class="detail">
+              <h4 class="menu_name">粒マスタードハンバーグ弁当</h4>
+              <p>
+                <span class="kakaku">本体価格 : 741円/1個</span>
+                <span class="zeikomi">(税込 : 800円/1個)</span>
+                <span class="energy">1210kcal / 45.1g / 56.1g / 124.9g / 1919mg / 4.9g</span>
+              </p>
+            </div>
+          </a>
+        </div>
+      </li>
+
+      <li class="item col-3 col-4-lg col-6-md col-12-sm">
+        <div class="item_content cm0">
+          <a href="#" class="link">
+            <div class="figure">
+              <figure><img src="https://placehold.jp/900x600.png" alt=""></figure>
+              <span class="label cm0">new</span>
+            </div>
+            <div class="line"></div>
+            <div class="detail">
+              <h4 class="menu_name">粒マスタードハンバーグ弁当</h4>
+              <p>
+                <span class="kakaku">本体価格 : 741円/1個</span>
+                <span class="zeikomi">(税込 : 800円/1個)</span>
+                <span class="energy">1210kcal / 45.1g / 56.1g / 124.9g / 1919mg / 4.9g</span>
+              </p>
+            </div>
+          </a>
+        </div>
+      </li>
+
+      <li class="item col-3 col-4-lg col-6-md col-12-sm">
+        <div class="item_content cm0">
+          <a href="#" class="link">
+            <div class="figure">
+              <figure><img src="https://placehold.jp/900x600.png" alt=""></figure>
+              <span class="label cm0">new</span>
+            </div>
+            <div class="line"></div>
+            <div class="detail">
+              <h4 class="menu_name">粒マスタードハンバーグ弁当</h4>
+              <p>
+                <span class="kakaku">本体価格 : 741円/1個</span>
+                <span class="zeikomi">(税込 : 800円/1個)</span>
+                <span class="energy">1210kcal / 45.1g / 56.1g / 124.9g / 1919mg / 4.9g</span>
+              </p>
+            </div>
+          </a>
+        </div>
+      </li>
+
+      <li class="item col-3 col-4-lg col-6-md col-12-sm">
+        <div class="item_content cm0">
+          <a href="#" class="link">
+            <div class="figure">
+              <figure><img src="https://placehold.jp/900x600.png" alt=""></figure>
+              <span class="label cm0">new</span>
+            </div>
+            <div class="line"></div>
+            <div class="detail">
+              <h4 class="menu_name">粒マスタードハンバーグ弁当</h4>
+              <p>
+                <span class="kakaku">本体価格 : 741円/1個</span>
+                <span class="zeikomi">(税込 : 800円/1個)</span>
+                <span class="energy">1210kcal / 45.1g / 56.1g / 124.9g / 1919mg / 4.9g</span>
+              </p>
+            </div>
+          </a>
+        </div>
+      </li>
+
+      <li class="item col-3 col-4-lg col-6-md col-12-sm">
+        <div class="item_content cm0">
+          <a href="#" class="link">
+            <div class="figure">
+              <figure><img src="https://placehold.jp/900x600.png" alt=""></figure>
+              <span class="label cm0">new</span>
+            </div>
+            <div class="line"></div>
+            <div class="detail">
+              <h4 class="menu_name">粒マスタードハンバーグ弁当</h4>
+              <p>
+                <span class="kakaku">本体価格 : 741円/1個</span>
+                <span class="zeikomi">(税込 : 800円/1個)</span>
+                <span class="energy">1210kcal / 45.1g / 56.1g / 124.9g / 1919mg / 4.9g</span>
+              </p>
+            </div>
+          </a>
+        </div>
+      </li>
+
+      <li class="item col-3 col-4-lg col-6-md col-12-sm">
+        <div class="item_content cm0">
+          <a href="#" class="link">
+            <div class="figure">
+              <figure><img src="https://placehold.jp/900x600.png" alt=""></figure>
+              <span class="label cm0">new</span>
+            </div>
+            <div class="line"></div>
+            <div class="detail">
+              <h4 class="menu_name">粒マスタードハンバーグ弁当</h4>
+              <p>
+                <span class="kakaku">本体価格 : 741円/1個</span>
+                <span class="zeikomi">(税込 : 800円/1個)</span>
+                <span class="energy">1210kcal / 45.1g / 56.1g / 124.9g / 1919mg / 4.9g</span>
+              </p>
+            </div>
+          </a>
+        </div>
+      </li>
+
+    </ul>
+  </div>
+</section>
 <?php get_footer(); ?>
